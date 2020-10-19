b29dbd808e129382f032c1c200e33ddb458835b7
villasurlamer <loic.dekester@gmail.com>
2020-10-19 15:53:27 -0700

    all post component

--- src/app/all-posts/all-posts.component.ts
+++ src/app/all-posts/all-posts.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-all-posts',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-posts.component.css']
 })
 export class AllPostsComponent implements OnInit {
+  allRef: any;
+  loadMoreRef: any;
+  allPosts: any = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.allRef = firebase.database().ref('allposts').limitToFirst(3);
+    this.allRef.on('child_added', data => {
+      this.allPosts.push({
+        key: data.key,
+        data: data.val()
+      });
+    });
+  }
+
+  onLoadMore() {
+    if (this.allPosts.length > 0) {
+      const lastLoadedPost = this.allPosts[this.allPosts.length - 1];
+      const lastLoadedPostKey = lastLoadedPost.key;
+      this.loadMoreRef = firebase.database().ref('allposts').startAt(null, lastLoadedPostKey).limitToFirst(3 + 1);
+      this.loadMoreRef.on('child_added', data => {
+        if (data.key === lastLoadedPostKey) {
+          return;
+        } else {
+          this.allPosts.push({
+            key: data.key,
+            data: data.val()
+          });
+        }
+      });
+
+    }
+  }
+
+  ngOnDestroy() {
+    this.allRef.off();
   }
 
 }
